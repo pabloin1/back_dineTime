b2a76eb7538a242204f2e20a2b862ccfd032689c
pabloin1 <pablocesaraltuzar04@gmail.com>
2024-07-01 21:13:33 -0600

    cambiar tipo a producto

---
+++ prisma/migrations/20240702031102_cambiar_campo_tipo_de_categoria_a_producto/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tipo` on the `categorias` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[tipo]` on the table `Producto` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `tipo` to the `Producto` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX `Categorias_tipo_key` ON `categorias`;
+
+-- AlterTable
+ALTER TABLE `categorias` DROP COLUMN `tipo`;
+
+-- AlterTable
+ALTER TABLE `producto` ADD COLUMN `tipo` VARCHAR(191) NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Producto_tipo_key` ON `Producto`(`tipo`);
