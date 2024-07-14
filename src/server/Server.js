d7684aa238350b9311eb0e7bcca0134cde076f83
pabloin1 <pablocesaraltuzar04@gmail.com>
2024-07-14 17:16:45 -0600

    api terminada con https

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,13 +1,16 @@
-import bodyParser from "body-parser";
-import express from "express";
-import cors from "cors";
-import routes from "../routes/index.js";
-import { validarApiKey } from "../middlewares/validar-ApiKey.js";
+import https from 'https';
+import fs from 'fs';
+import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+import routes from '../routes/index.js';
+import { validarApiKey } from '../middlewares/validar-ApiKey.js';
 
 export class ServerApi {
   constructor() {
     this.app = express();
-    this.port = 3000 || 8080;
+    this.port = process.env.PORT || 3000;
+    this.httpsPort = 443;
 
     // Middlewares
     this.middlewares();
@@ -30,8 +33,17 @@ export class ServerApi {
   }
 
   listen() {
-    this.app.listen(this.port, () =>
+    this.app.listen(this.port, () => 
       console.log(`Escuchando en el puerto ${this.port}`)
     );
+
+    const options = {
+      key: fs.readFileSync('path/to/selfsigned.key'),
+      cert: fs.readFileSync('path/to/selfsigned.crt')
+    };
+
+    https.createServer(options, this.app).listen(this.httpsPort, () => {
+      console.log(`Server is running on https://your-ec2-public-dns`);
+    });
   }
 }
