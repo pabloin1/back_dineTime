1933e2a4cd5c1652ddb5b81f3e95e73356b491a3
pabloin1 <pablocesaraltuzar04@gmail.com>
2024-07-14 21:43:16 -0600

    agregar https configuracion

--- src/server/Server.js
+++ src/server/Server.js
@@ -1,11 +1,15 @@
-import https from "https";
-import fs from "fs";
-import path from "path";
-import bodyParser from "body-parser";
-import express from "express";
-import cors from "cors";
-import routes from "../routes/index.js";
-import { validarApiKey } from "../middlewares/validar-ApiKey.js";
+import https from 'https';
+import fs from 'fs';
+import path from 'path';
+import bodyParser from 'body-parser';
+import express from 'express';
+import cors from 'cors';
+import routes from '../routes/index.js';
+import { fileURLToPath } from 'url';
+import { validarApiKey } from '../middlewares/validar-ApiKey.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
 
 export class ServerApi {
   constructor() {
@@ -28,25 +32,24 @@ export class ServerApi {
   }
 
   routes() {
-    routes.forEach((route) => {
+    routes.forEach(route => {
       this.app.use(route.path, route.router);
     });
   }
 
   listen() {
+    // Iniciar servidor HTTP
+    this.app.listen(this.port, () => {
+      console.log(`Escuchando en el puerto ${this.port}`);
+    });
+
     const httpsOptions = {
-      key: fs.readFileSync(
-        path.join(__dirname, "../../../etc/nginx/ssl/nginx-selfsigned.key")
-      ),
-      cert: fs.readFileSync(
-        path.join(__dirname, "../../../etc/nginx/ssl/nginx-selfsigned.crt")
-      ),
-    };
+      key: fs.readFileSync(path.join(__dirname, '../../../etc/nginx/ssl/nginx-selfsigned.key')),
+      cert: fs.readFileSync(path.join(__dirname, '../../../etc/nginx/ssl/nginx-selfsigned.crt'))
+  };
 
     https.createServer(httpsOptions, this.app).listen(this.httpsPort, () => {
-      console.log(
-        `Server is running on https://your-ec2-public-dns:${this.httpsPort}`
-      );
+      console.log(`Server is running on https://your-ec2-public-dns:${this.httpsPort}`);
     });
   }
 }
